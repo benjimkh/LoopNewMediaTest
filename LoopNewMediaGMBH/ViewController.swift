dc009dac37e023c3f8897a458108226418e48949
Benyamin Mokhtarpour <benyamin.mkh@gmail.com>
2020-09-19 23:04:23 +0430

    Initial Commit

---
+++ LoopNewMediaGMBH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoopNewMediaGMBH
+//
+//  Created by Benyamin Mokhtarpour on 9/19/20.
+//  Copyright © 2020 Benyamin Mokhtarpour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
